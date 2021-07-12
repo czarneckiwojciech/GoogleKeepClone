c2333bab0c6f6590b8a0322e2f8740c66127e607
czarneckiwojciech <wojtekczarnnecki@gmail.com>
2021-07-12 19:48:58 +0200

    new feature

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// const noteTitleElement = document.querySelector('.note-title');
+let allNotesLists = [];
+
+const noteTitleElement = document.querySelector('.note-title');
 const noteDescriptionContainerElement = document.querySelector('.note-description-container');
-
 const inputForNameTitle = document.querySelector('.create-note-title');
-
 const initailInput = document.querySelector('.create-note')
-
 const notepadWrapper = document.querySelector('.notepad-wrapper')
 const textNoteArea = document.querySelector('.note-content')
 const notesListElement = document.querySelector('.notes-list-container');
@@ -20,7 +19,7 @@ initailInput.addEventListener('click', () => {
   
   if (noteDescriptionContainerElement.classList.contains('hidden')) {
     noteDescriptionContainerElement.classList.toggle('hidden')
-    // noteTitleWithPlaceHolderTextTitle.classList.toggle('hidden')
+    
   }
 
   if (inputForNameTitle.classList.contains('hidden')) {
@@ -30,41 +29,11 @@ initailInput.addEventListener('click', () => {
   focusFunction()
 })
 
-document.addEventListener('click', (event) => {
-  if (!event.target.closest('.notepad-wrapper') || event.target.matches('.close-btn')) {
-    colapseNote()
-    createNote()
-    clearInputs()
-  }
-})
-
 const colapseNote = () => {
   noteDescriptionContainerElement.classList.add('hidden');
   inputForNameTitle.classList.add('hidden');
   initailInput.classList.remove('hidden');
   notepadWrapper.classList.remove('border')
-}
-
-const createNote = (note) => {
-
- let valueOfNoteTextContentElement = textNoteArea.value;
-  let valueOfNoteTitleElement = inputForNameTitle.value;
-  
-  if (valueOfNoteTextContentElement || valueOfNoteTitleElement) {
-    
-  } else if (valueOfNoteTextContentElement === '' || valueOfNoteTitleElement === '') {
-   return
-  }
- 
- /*  if (noteDescriptionValue === '' || noteTitleElementValue === '') {
-    return 
-  } */
-
-  console.log(valueOfNoteTextContentElement)
-  console.log(valueOfNoteTitleElement)
-  renderNote(valueOfNoteTitleElement,valueOfNoteTextContentElement)
-  
-  /* console.log(renderNote(noteDescriptionValue)) */
 }
 
 const clearInputs = () => {
@@ -76,20 +45,74 @@ const focusFunction = () => {
   textNoteArea.focus();
 }
 
-const renderNote = (valueOfNoteTitleElement, valueOfNoteTextContentElement) => {
-  
-  const noteElement = document.createElement('div');
-  const noteTitleElement = document.createElement('h3');
-  const noteTextContentElement = document.createElement('p');
-
-  noteElement.className = 'note-element';
-  noteTitleElement.className = 'note-title';
-  noteTextContentElement.className = 'note-text-box';
-
-  notesListElement.appendChild(noteElement);
-  noteElement.appendChild(noteTitleElement);
-  noteElement.appendChild(noteTextContentElement);
-  
-  noteTitleElement.innerHTML = valueOfNoteTitleElement;
-  noteTextContentElement.innerHTML = valueOfNoteTextContentElement;
+const createNoteTemplate = (note) => {
+  const noteTemplate =
+    `<div id="noteElement" class="note-element">
+      <h3 class="note-title">${note.title}</h3>
+      <p class="note-text-box">${note.text}</p>
+      <i id="${note.id}" class="fas fa-trash trash-btn"></i> 
+    </div>`
+  return noteTemplate
 }
+
+const createNewNote = (noteTitle,noteText) => {
+  const note = {
+    title: noteTitle,
+    text: noteText,
+    id: Math.random()
+  }
+  allNotesLists.unshift(note)
+}
+
+const removeNote = (noteId) => {
+  allNotesLists = allNotesLists.filter((note) => {
+    return noteId !== String(note.id)
+  })
+}
+
+const addRemoveListenersToBtns = () => {
+  const allRemoveBtnsElements = document.querySelectorAll('.trash-btn')
+
+  allRemoveBtnsElements.forEach((removeBtn) => {
+    removeBtn.addEventListener('click', ($event) => {
+      const noteId = $event.target.attributes.id.nodeValue;
+
+      removeNote(noteId);
+      renderNotes();
+    })
+  })
+}
+
+const renderNotes = () => {
+  const notesListElement = document.querySelector('.notes-list-container');
+
+  const notesTemplates = allNotesLists.map((note) => {
+    return createNoteTemplate(note);
+  })
+  notesListElement.innerHTML = notesTemplates;
+  addRemoveListenersToBtns()
+}
+
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('.notepad-wrapper') || event.target.matches('.close-btn')) {
+    
+    let noteText = textNoteArea.value;
+    let noteTitle = inputForNameTitle.value;
+    
+    if (noteText === '' && noteTitle === '') {
+      colapseNote()
+    } else {
+    createNewNote(noteTitle, noteText)
+    renderNotes()
+    colapseNote()
+    clearInputs()
+    }
+  }
+})
+
+
+/* const noteElement = document.getElementById('noteElement') ;
+
+noteElement.addEventListener('click', () => {
+  console.log('działa hover?')
+}) */
